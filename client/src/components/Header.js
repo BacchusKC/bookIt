e1ee3086abc31a5bd80373306a35e60f89512573
BacchusKC <bacchuskc@gmail.com>
2019-04-19 15:32:19 -0500

    NavBar redo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,17 +6,9 @@ function Header(props) {
     return (
         <div className="header">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">Book It</a>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link to="/search" className="nav-link">Search</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/saved" className="nav-link">Saved</Link>
-                        </li>
-                    </ul>
-                </div>
+                <Link to="/" className="navbar-brand">Book It</Link>
+                <Link to="/search" className="nav-link">Search</Link>
+                <Link to="/saved" className="nav-link">Saved</Link>
             </nav>
         </div>
     );
