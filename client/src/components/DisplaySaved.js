5d1b742d4130f337d584e15289d9e5fc9b362159
BacchusKC <bacchuskc@gmail.com>
2019-04-20 00:09:06 -0500

    Now Finished, new navbar

--- client/src/components/DisplaySaved.js
+++ client/src/components/DisplaySaved.js
@@ -6,7 +6,7 @@ function DisplaySaved(props) {
         <div className="media bookDiv">
             <img className="align-self-start mr-3" src={props.image} alt={props.title} />
             <a href={props.saleLink} className="viewButton" target="_blank" rel="noopener noreferrer"><button className="button">View</button></a>
-                <button id={props.id} className="deleteButton" onClick={(event) => props.handleDelete(event)}>Delete</button>
+            <button id={props.id} className="deleteButton" onClick={(event) => props.handleDelete(event)}>Delete</button>
             <div className="media-body">
                 <h4 className="mt-0"><strong>{props.title}</strong></h4>
                 <h5>{props.authors}</h5>
